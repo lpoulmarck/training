277b2f7430ce73053dcf7133914698390c4aa98f
lpoulmarck <31709836+lpoulmarck@users.noreply.github.com>
2017-09-28 03:18:25 +0100

    used escape characters for better presentation

--- numericalSystems/main.go
+++ numericalSystems/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	for i := 0; i < 10; i++ { // i++ just means add 1 to i
-		fmt.Printf("%d %b", i, i)
+		fmt.Printf("%d \t %b \n", i, i) // \t means horizontal tab and \n means new line
 	}
 
 }
